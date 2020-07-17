0efce3522b86d4292689b9b33c6c04725d9dfa4d
GDuke <gdsolutions@protonmail.com>
2020-07-17 15:20:31 +0100

    Increase readability, implement selection sort

---
+++ algorithms/sorts/selection-sort.js
@@ -0,0 +1,24 @@
+// This program implements an ascending insertion sort
+// This is part of my solutions to Colt Steel's Algorithms and Data Structures course
+
+function selectionSort(sortingArray) {
+  arr = sortingArray; // Avoid mutating the original array
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      min = arr[i];
+      minIndex = i;
+
+      if (min > arr[j]) {
+        min = arr[j];
+        minIndex = j;
+      }
+
+      temp = arr[i];
+      arr[i] = min;
+      arr[minIndex] = temp;
+    }
+  }
+
+  return arr;
+}
